866ba852677ba85b2f9fbade76b706ca074a3dc7
canwdev <c2097@live.com>
2023-09-13 18:33:07 +0800

    migrate to vue3

--- publish-gh-pages.sh
+++
@@ -1,8 +0,0 @@
-yarn run build
-cd dist
-git init
-git add .
-git commit -m "update"
-git branch -m master gh-pages
-git remote add origin https://gitee.com/canwdev/garageband-piano-js.git
-git push -f origin gh-pages
